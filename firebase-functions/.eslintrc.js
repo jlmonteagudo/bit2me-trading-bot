e26d07f85cbe8ce0c89174ac94f9a6218918d61a
Jose Luis Monteagudo <jl_monteagudo@yahoo.es>
2024-05-09 20:55:56 +0200

    feat: create candles firebase function

---
+++ firebase-functions/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.*'],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
---
+++ firebase-functions/index.js
@@ -0,0 +1,41 @@
+const { onRequest } = require('firebase-functions/v2/https');
+const logger = require('firebase-functions/logger');
+const fetch = require('node-fetch');
+
+const SERVER_URL = 'https://gateway.bit2me.com';
+const BASE_PATH = '/v1/trading';
+
+exports.helloWorld = onRequest((request, response) => {
+  logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
+});
+
+exports.candles = onRequest(async (request, response) => {
+  const { symbol, interval, numberOfCandles } = request.query;
+  const endTime = getEndTime();
+  const startTime = getStartTime(interval, numberOfCandles, endTime);
+
+  let candles = [];
+
+  try {
+    // eslint-disable-next-line max-len
+    const url = `${BASE_PATH}/candle?symbol=${symbol}&interval=${interval}&startTime=${startTime}&endTime=${endTime}&limit=1000`;
+    const response = await fetch(`${SERVER_URL}${url}`);
+    const json = await response.json();
+    candles = json.filter((candle) => candle[0] >= startTime);
+  } catch (error) {
+    candles = [];
+  }
+
+  response.send(candles);
+});
+
+const getEndTime = () => {
+  return new Date().getTime();
+};
+
+const getStartTime = (interval, numberOfCandles, endTime) => {
+  const intervalMilliseconds = interval * numberOfCandles * 60 * 1000;
+  const startTime = endTime - intervalMilliseconds;
+  return startTime;
+};
