f5558ca97b511717db66222bf069584f0deec558
Monteagudo <jl_monteagudo@yahoo.es>
2024-12-27 15:39:08 +0100

    feat: Does not broadcast empty log

--- src/domains/trading-strategies/calculate-most-performant-markets-with-ema-and-rsi.js
+++ src/domains/trading-strategies/calculate-most-performant-markets-with-ema-and-rsi.js
@@ -38,7 +38,6 @@ export const calculateMostPerformantMarketsWithEMAAndRSI = async () => {
   await savePerformantMarkets(rankedTickers);
 
   logger.info(`Found ${rankedTickers.length} performant markets: ${JSON.stringify(rankedTickers.map(t => t.ticker.symbol))}`);
-  broadcastLog('');
 };
 
 const isMarketPerformant = async (symbol) => {
