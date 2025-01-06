cc51988e10028ca701e08f3e4a321b395ea4212b
Monteagudo <jl_monteagudo@yahoo.es>
2025-01-06 18:37:10 +0100

    feat: import logger in load balance use case

--- src/domains/balances/use-cases/load-balances.usecase.js
+++ src/domains/balances/use-cases/load-balances.usecase.js
@@ -1,5 +1,6 @@
 import * as connector from '../../../conector/bit2me.js';
 import { saveBalances } from '../repository/balances.repository.js';
+import { logger } from '../../../core/logger/logger.js';
 
 const WATCH_INTERVAL_MILISECONDS = 60_000;
 
@@ -7,7 +8,6 @@ export const watchBalances = () => {
   loadBalances();
   setInterval(loadBalances, WATCH_INTERVAL_MILISECONDS);
 };
-
 
 export const loadBalances = async () => {
   try {
